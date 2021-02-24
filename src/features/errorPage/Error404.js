baeacb98fbc82aa9b6a5b256a9bc2965c2999e63
Eric Masi <eric.masi90@gmail.com>
2021-02-23 19:14:23 -0500

    Initial commit

---
+++ src/features/errorPage/Error404.js
@@ -0,0 +1,16 @@
+// Modules
+import React from 'react';
+
+// Styles
+import styles from './Error404.module.css';
+
+function Error404() {
+  return (
+    <div className={styles.container}>
+      <h1>Page Not Found</h1>
+      <p>Sorry, there are no memes or stocks to see here</p>
+    </div>
+  );
+}
+
+export default Error404;
