a31b48fd9504394379a241be0094b117f8f1c210
Eric Masi <eric.masi90@gmail.com>
2021-02-27 11:05:42 -0500

    Add transactions feed route and basic component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import React from 'react';
 // Components
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Error404 from './features/errorPage/Error404';
+import Transactions from './features/transactionsFeed/Transactions';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/transactions/feed">
+          <Transactions />
+        </Route>
         <Route path="/">
           <Error404 />
         </Route>
