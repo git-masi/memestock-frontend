897418bc1063be45f14beed95bb0402191071652
Benjamin Carlson <hellobencarlson@gmail.com>
2021-03-13 14:03:17 -0500

    icons showing up

--- src/features/sidebar/Sidebar.js
+++ src/features/sidebar/Sidebar.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentsDollar } from '@fortawesome/free-solid-svg-icons';
+import { faHistory } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 export default class Sidebar extends Component {
 
@@ -23,30 +32,30 @@ export default class Sidebar extends Component {
         return (
             <React.Fragment>
               <div className="nav-container">
-                <i class="fas fa-chevron-circle-left" onClick="collapse()"></i>
-                <i class="fas fa-chevron-circle-right" onClick="uncollapse()"></i>
+              <FontAwesomeIcon icon={faChevronCircleLeft} onClick="collapse()" />
+              <FontAwesomeIcon icon={faChevronCircleRight} onClick="uncollapse()" />
                    {/* Sidebar icons and Links */}
                    <div className="sidebar-nav">
                     <ul className="sidebar-list">
                         <li>
-                            <i className="fas fa-comments-dollar"></i>
-                            <NavLink to="/feed">Feed</NavLink>
+                            <FontAwesomeIcon icon={faCommentsDollar} />
+                            <NavLink to="/feed" className="sidebar-navlink">Feed</NavLink>
                         </li>
                         <li>
-                            <i class="fas fa-chart-line"></i>
-                            <NavLink to="/visualize">Visualize</NavLink>
+                            <FontAwesomeIcon icon={faChartLine} />
+                            <NavLink to="/visualize" className="sidebar-navlink">Visualize</NavLink>
                         </li>
                         <li>
-                            <i class="fas fa-history"></i>
-                            <NavLink to="/history">History</NavLink>
+                            <FontAwesomeIcon icon={faHistory} />
+                            <NavLink to="/history" className="sidebar-navlink">History</NavLink>
                         </li>
                         <li>
-                            <i class="fas fa-plus-circle"></i>
-                            <NavLink to="/new-order">New Order</NavLink>
+                            <FontAwesomeIcon icon={faPlusCircle} />
+                            <NavLink to="/new-order" className="sidebar-navlink">New Order</NavLink>
                         </li>
                         <li>
-                            <i class="fas fa-cog"></i>
-                            <NavLink to="/settings" className="settings-navlink">Settings</NavLink>
+                            <FontAwesomeIcon icon={faCog} />
+                            <NavLink to="/settings" className="sidebar-navlink">Settings</NavLink>
                         </li>
                     </ul>
                 </div>
