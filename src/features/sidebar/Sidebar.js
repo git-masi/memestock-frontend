0c95e347138c75b89e03215fbaf1f8fd9b5dd1d6
Eric Masi <eric.masi90@gmail.com>
2021-03-16 10:05:12 -0400

    Refactor to make icons clickable

--- src/features/sidebar/Sidebar.js
+++ src/features/sidebar/Sidebar.js
@@ -24,61 +24,68 @@ export default function Sidebar() {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <p style={{ visibility: 'hidden' }}>hidden</p>
         {openSidebar ? (
           <FontAwesomeIcon
             icon={faChevronCircleLeft}
             onClick={() => setOpenSidebar(false)}
-            className={styles.icon}
+            className={[styles.icon, styles.chevron].join(' ')}
             size="2x"
           />
         ) : (
           <FontAwesomeIcon
             icon={faChevronCircleRight}
             onClick={() => setOpenSidebar(true)}
-            className={styles.icon}
+            className={[styles.icon, styles.chevron].join(' ')}
             size="2x"
           />
         )}
 
         {/* todo: conditionally render based on global singed in state */}
         <NavLink to="/sign-in" className={styles.link}>
-          Sign In
+          <span>Sign In</span>
+          <FontAwesomeIcon
+            icon={faSignInAlt}
+            className={styles.icon}
+            size="2x"
+          />
         </NavLink>
-        <FontAwesomeIcon icon={faSignInAlt} className={styles.icon} size="2x" />
 
         <NavLink to="/feed" className={styles.link}>
           Feed
+          <FontAwesomeIcon
+            icon={faCommentsDollar}
+            className={styles.icon}
+            size="2x"
+          />
         </NavLink>
-        <FontAwesomeIcon
-          icon={faCommentsDollar}
-          className={styles.icon}
-          size="2x"
-        />
 
         <NavLink to="/visualize" className={styles.link}>
           Visualize
+          <FontAwesomeIcon
+            icon={faChartLine}
+            className={styles.icon}
+            size="2x"
+          />
         </NavLink>
-        <FontAwesomeIcon icon={faChartLine} className={styles.icon} size="2x" />
 
         <NavLink to="/history" className={styles.link}>
           History
+          <FontAwesomeIcon icon={faHistory} className={styles.icon} size="2x" />
         </NavLink>
-        <FontAwesomeIcon icon={faHistory} className={styles.icon} size="2x" />
 
         <NavLink to="/new-order" className={styles.link}>
           New Order
+          <FontAwesomeIcon
+            icon={faPlusCircle}
+            className={styles.icon}
+            size="2x"
+          />
         </NavLink>
-        <FontAwesomeIcon
-          icon={faPlusCircle}
-          className={styles.icon}
-          size="2x"
-        />
 
         <NavLink to="/settings" className={styles.link}>
           Settings
+          <FontAwesomeIcon icon={faCog} className={styles.icon} size="2x" />
         </NavLink>
-        <FontAwesomeIcon icon={faCog} className={styles.icon} size="2x" />
       </div>
     </div>
   );
