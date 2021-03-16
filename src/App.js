9ec9883f592921a412746588032b34d1d4d7b30c
Eric Masi <47583837+git-masi@users.noreply.github.com>
2021-03-16 10:21:37 -0400

    Eric sidebar refactor (#8)
    * Remove unused code, run prettier
    
    * Consolidate imports to clean up code
    
    * Add css module
    
    * Remove excess JSX elements
    
    * Position elements
    
    * Refactor to make icons clickable
    
    * Add active class styles
    
    * Remove unused code
    
    * Change path to reflect nav link
    
    * Animate sidebar
    
    * Edit styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 // Modules
 import React from 'react';
 
+// Styles
+import styles from './App.module.css';
+
 // Components
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Error404 from './features/errorPage/Error404';
 import TransactionsFeed from './features/transactionsFeed/TransactionsFeed';
 import GlobalLoader from './features/portal/GlobalLoader';
 import Sidebar from './features/sidebar/Sidebar';
-import './features/sidebar/sidebar.css';
 
 function App() {
   return (
     <>
       <GlobalLoader />
       <Router>
-        <Sidebar />
-        <Switch>
-          <Route path="/transactions/feed">
-            <TransactionsFeed />
-          </Route>
-          <Route path="/">
-            <Error404 />
-          </Route>
-        </Switch>
+        <main className={styles.main}>
+          <Sidebar />
+          <Switch>
+            <Route path="/feed">
+              <TransactionsFeed />
+            </Route>
+            <Route path="/">
+              <Error404 />
+            </Route>
+          </Switch>
+        </main>
       </Router>
     </>
   );
