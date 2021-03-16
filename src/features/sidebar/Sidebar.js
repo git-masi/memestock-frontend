8a91dd87f0a8f594711229d23cd9516852a19d7d
Benjamin Carlson <bcarlso1@gmail.com>
2021-03-16 08:52:46 -0400

    Ben c sidebar 2 (#7)
    * initial sidebar code
    
    * icons showing up
    
    * sidebar styling and collapse functionality
    
    * refactored with hooks
    
    Co-authored-by: Eric Masi <47583837+git-masi@users.noreply.github.com>

--- src/features/sidebar/Sidebar.js
+++ src/features/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCommentsDollar } from '@fortawesome/free-solid-svg-icons';
@@ -9,51 +9,54 @@ import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 
+export default function Sidebar() {
 
-export default class Sidebar extends Component {
-
-    componentDidMount() {
-        collapse();
-    }
-
-    render() {
+    const [openSidebar, setOpenSidebar] = useState(false);
 
         return (
             <React.Fragment>
               <div className="nav-container">
-              <FontAwesomeIcon icon={faChevronCircleLeft} onClick={collapse} className="collapse-sidebar" />
-              <FontAwesomeIcon icon={faChevronCircleRight} onClick={uncollapse} className="uncollapse-sidebar" />
-                   {/* Sidebar icons and Links */}
-                   <div className="sidebar-nav">
-                    <ul className="sidebar-list">
-                        <li>
-                            <NavLink to="/sign-up" className="sidebar-navlink sign-up">Sign Up</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/sign-in" className="sidebar-navlink sign-in">Sign In</NavLink>
-                        </li>
-                        <li>
-                            <FontAwesomeIcon icon={faCommentsDollar} />
-                            <NavLink to="/feed" className="sidebar-navlink">Feed</NavLink>
-                        </li>
-                        <li>
-                            <FontAwesomeIcon icon={faChartLine} />
-                            <NavLink to="/visualize" className="sidebar-navlink">Visualize</NavLink>
-                        </li>
-                        <li>
-                            <FontAwesomeIcon icon={faHistory} />
-                            <NavLink to="/history" className="sidebar-navlink">History</NavLink>
-                        </li>
-                        <li>
-                            <FontAwesomeIcon icon={faPlusCircle} />
-                            <NavLink to="/new-order" className="sidebar-navlink">New Order</NavLink>
-                        </li>
-                        <li>
-                            <FontAwesomeIcon icon={faCog} />
-                            <NavLink to="/settings" className="sidebar-navlink">Settings</NavLink>
-                        </li>
-                    </ul>
-                </div>
+              {
+                    openSidebar &&
+                    <FontAwesomeIcon icon={faChevronCircleLeft} onClick={() => setOpenSidebar(false)} className="collapse-sidebar" />
+              }
+              {
+                    !openSidebar &&
+              <FontAwesomeIcon icon={faChevronCircleRight} onClick={() => setOpenSidebar(true)} className="uncollapse-sidebar" />
+              } 
+              {
+                    openSidebar &&
+                        <div className="sidebar-nav">
+                            <ul className="sidebar-list">
+                                <li>
+                                    <NavLink to="/sign-up" className="sidebar-navlink sign-up">Sign Up</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/sign-in" className="sidebar-navlink sign-in">Sign In</NavLink>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faCommentsDollar} />
+                                    <NavLink to="/feed" className="sidebar-navlink">Feed</NavLink>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faChartLine} />
+                                    <NavLink to="/visualize" className="sidebar-navlink">Visualize</NavLink>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faHistory} />
+                                    <NavLink to="/history" className="sidebar-navlink">History</NavLink>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faPlusCircle} />
+                                    <NavLink to="/new-order" className="sidebar-navlink">New Order</NavLink>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faCog} />
+                                    <NavLink to="/settings" className="sidebar-navlink">Settings</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    }
             </div>
 
             </React.Fragment>
@@ -62,19 +65,3 @@ export default class Sidebar extends Component {
         )
 
     }
-
-   
-
-}
-
-function collapse() {
-document.getElementsByClassName("sidebar-nav")[0].style.display = "none";
-document.getElementsByClassName("collapse-sidebar")[0].style.display = "none";
-document.getElementsByClassName("uncollapse-sidebar")[0].style.display = "block";
-}
-
-function uncollapse() {
-    document.getElementsByClassName("sidebar-nav")[0].style.display = "block";
-    document.getElementsByClassName("uncollapse-sidebar")[0].style.display = "none";
-    document.getElementsByClassName("collapse-sidebar")[0].style.display = "block";
-}
