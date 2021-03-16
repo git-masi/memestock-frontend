eda004bb1305fe99e795e5a69a706cd7b85e84a6
Eric Masi <eric.masi90@gmail.com>
2021-03-16 09:05:47 -0400

    Add css module

--- src/features/sidebar/Sidebar.js
+++ src/features/sidebar/Sidebar.js
@@ -1,4 +1,10 @@
+// Modules
 import React, { useState } from 'react';
+
+// Styles
+import styles from './Sidebar.module.css';
+
+// Components
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
