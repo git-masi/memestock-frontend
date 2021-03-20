4d2fe4cc7bd16a152be7c93f0cd33c2ec65722cd
Eric Masi <47583837+git-masi@users.noreply.github.com>
2021-03-20 12:09:23 -0400

    Stock picker refactor (#10)
    * Run prettier and change folder name to match naming convention
    
    * Remove for loop to make code more terse
    
    * Minimize markup, use semantic tags, edit styles
    
    * Add clarifying comment

--- src/features/StockTicker/StockTicker.js
+++ src/features/StockTicker/StockTicker.js
@@ -7,100 +7,81 @@ import styles from './StockTicker.module.css';
 
 // Components
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faSortUp,
-    faSortDown
-} from '@fortawesome/free-solid-svg-icons';
+import { faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
+// sample data
+const stocksArray = [
+  {
+    stockSymbol: 'GMSK',
+    stockPrice: '$102.31',
+    priceChange: '$-0.09',
+    transactionMessage: 'Buy and hold',
+    id: 'bd75dc21-bef6-4c90-ae3e-f1f9b26faff3',
+  },
+  {
+    stockSymbol: 'MDPC',
+    stockPrice: '$32.91',
+    priceChange: '$0.06',
+    transactionMessage: 'Sleeper',
+    id: 'ee93ca35-25fd-438a-bfa3-3f83d4298018',
+  },
+  {
+    stockSymbol: 'OTHR',
+    stockPrice: '$12.45',
+    priceChange: '$-0.20',
+    transactionMessage: 'Never know',
+    id: 'dbc3605c-d6d0-4138-8710-01f19eb1087d',
+  },
+  {
+    stockSymbol: 'HYPE',
+    stockPrice: '$112.05',
+    priceChange: '$0.49',
+    transactionMessage: 'Trending on Twitter',
+    id: '85f0e98d-ec8c-4392-970d-6fe050003c83',
+  },
+  {
+    stockSymbol: 'MEME',
+    stockPrice: '$286.76',
+    priceChange: '$0.98',
+    transactionMessage: 'To the moon',
+    id: '8a6f6bd1-ae10-4f58-81fd-55dad72d019e',
+  },
+];
 
 export default function StockTicker() {
+  return (
+    <section className={styles.container}>
+      <Ticker mode="await">
+        {() => (
+          <>
+            {stocksArray.map((stock) => {
+              return (
+                <article className={styles.stock} key={stock.id}>
+                  <h4>{stock.stockSymbol}</h4>
 
-    // sample data
-    var stocksArray = [
-        {
-            stockSymbol: "GMSK",
-            stockPrice: "$102.31",
-            latestMove: "$-0.09",
-            transactionMessage: "Buy and hold"
-        },
-        {
-            stockSymbol: "MDPC",
-            stockPrice: "$32.91",
-            latestMove: "$0.06",
-            transactionMessage: "Sleeper"
-        },
-        {
-            stockSymbol: "OTHR",
-            stockPrice: "$12.45",
-            latestMove: "$-0.20",
-            transactionMessage: "Never know"   
-        },
-        {
-            stockSymbol: "HYPE",
-            stockPrice: "$112.05",
-            latestMove: "$0.49",
-            transactionMessage: "Trending on Twitter"
-        },
-        {
-            stockSymbol: "MEME",
-            stockPrice: "$286.76",
-            latestMove: "$0.98",
-            transactionMessage: "To the moon"
-        }
-    ]
+                  <p>{stock.stockPrice}</p>
 
-    // build ticker object from data
-    let tickerObject = []
-    
-    for (var i = 0; i < stocksArray.length; i++) {
-        let upDown;
-        let removeDollarSign = stocksArray[i].latestMove.substring(1);
-        if (parseFloat(removeDollarSign) > 0) {
-            upDown =  
-                <FontAwesomeIcon
-                    icon={faSortUp}
-                    className={styles.upIcon}
-                /> 
-        } else {
-            upDown = 
-            <FontAwesomeIcon
-                icon={faSortDown}
-                className={styles.downIcon}
-            /> 
-        }
-        
-        tickerObject[i] = 
-            <div>
-                <div className={styles.stockTitle}>
-                    {stocksArray[i].stockSymbol}
-                </div>
-                <div>
-                    {stocksArray[i].stockPrice}
-                </div>
-                <div>
-                 {upDown}
-                    {stocksArray[i].latestMove} 
-                </div>
-                <div>
-                    "{stocksArray[i].transactionMessage}"
-                </div>
-            </div>
-        }
+                  {+stock.priceChange.replace('$', '') >= 0 ? (
+                    <FontAwesomeIcon
+                      icon={faSortUp}
+                      className={styles.upIcon}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faSortDown}
+                      className={styles.downIcon}
+                    />
+                  )}
 
-        return (
-        <div className={styles.wrapper}>
-            <Ticker>
-                {({ index }) => (
-                    <>
-                       <div className={styles.tickerContainer}>
-                            {tickerObject}
-                       </div>
-                    </>
-                )}
-            </Ticker>
-        </div>
-        )
+                  <p>{stock.priceChange}</p>
 
-    }
-
-
+                  <p>"{stock.transactionMessage}"</p>
+                </article>
+              );
+            })}
+          </>
+        )}
+      </Ticker>
+    </section>
+  );
+}
