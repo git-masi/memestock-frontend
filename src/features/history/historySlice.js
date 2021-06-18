cbf261f51c716f88153166478f56891574c6d7f5
Eric Masi <47583837+git-masi@users.noreply.github.com>
2021-06-18 16:35:44 -0400

    Use v2 endpoints (#19)
    * Add new endpoint
    
    * Refactor to use new endpoint
    
    * Use new api url
    
    * Update user endpoints
    
    * Refactor to use new endpoints

--- src/features/history/historySlice.js
+++ src/features/history/historySlice.js
@@ -1,12 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const { REACT_APP_TRANSACTION_SERVICE_URL } = process.env;
+const { REACT_APP_MEMESTOCK_API } = process.env;
 const tempToken =
   'eyJraWQiOiIzeHhsNnRreU1KeHVCT3V0OStLdHFzOG9KYzVZbncyZUUyZTlcLzVVakg5RT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI1MGNiNGQxMC04ZTI1LTRmMTUtYjU0MS1jMmY1Y2U3ZTQ0YTgiLCJldmVudF9pZCI6ImVjYjgwZjZjLTc1MzMtNDRiMy1iM2M2LWEyMDA1ZjZkNmU1NiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MjAxNDE1NTksImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX3RWN2pSYVZ0VCIsImV4cCI6MTYyMDE0NTE1OSwiaWF0IjoxNjIwMTQxNTU5LCJqdGkiOiIxNDVkZjkyZC1kYmNmLTQzOTUtODY4Ni04ZjI1MGYzYmVhZGQiLCJjbGllbnRfaWQiOiJuYXByZWtzMDJhbWIybmFpMTgwMXNuOXY0IiwidXNlcm5hbWUiOiI1ODcwYTdjZC05YzAxLTQ5NjktYmQzYS1hMDQ5NzZhYzk0MjUifQ.Ih53Y_X4F1BX8pB29umHnDDhr-E5kKa5Tn4swncB7wT4VxJfW1AKhftUy9J9d19sKA5K2ibh5gLxosXBUFak1tjGkZkOBnJeJjnkYaSYbeuHyiXfx1wxygwyYYk0UkR8IV_dwzRpNR0pB6ANz-1SRuVsGikM-DlBBFKyiBPHxwRvAQaS0LMMnMvI4F1F2kGRrO7lPK34tsmBAyKRBXX9RvvUMvg8zsiwK4_adj2GRpVnuxSKNCYiyvXG-QPj2L5W-vgsU0N59_MMCOW7Gp151RrIw3PJ3QxPB04fewwmEQsvD-2zdMBqbKAoQt9EeX4o5DHrGBgrYU8Twx_2xJYhPw';
 
 const initialState = {
-  transactionsPerPage: 10,
+  ordersPerPage: 10,
   pages: {
     1: [],
   },
@@ -53,22 +53,19 @@ export function fetchTransactionsHistory() {
   return async (dispatch, getState) => {
     try {
       const {
-        history: { transactionsPerPage },
+        history: { ordersPerPage },
       } = getState();
 
       const {
         data: { count },
-      } = await axios.get(
-        `${REACT_APP_TRANSACTION_SERVICE_URL}/transaction/count`,
-        {
-          headers: { Authorization: tempToken },
-        }
-      );
+      } = await axios.get(`${REACT_APP_MEMESTOCK_API}/orders/count/fulfilled`, {
+        headers: { Authorization: tempToken },
+      });
 
-      const numPages = count / transactionsPerPage;
+      const numPages = count / ordersPerPage;
 
       const { data } = await axios.get(
-        `${REACT_APP_TRANSACTION_SERVICE_URL}/transaction/many?orderAsc=false&limit=${transactionsPerPage}`
+        `${REACT_APP_MEMESTOCK_API}/orders?orderStatus=fulfilled&asc=false&limit=${ordersPerPage}`
       );
 
       dispatch(init({ numPages, data }));
@@ -82,21 +79,21 @@ export function fetchPageForTransactionsHistory(pageNumber) {
   return async (dispatch, getState) => {
     try {
       const {
-        history: { transactionsPerPage, pages },
+        history: { ordersPerPage, pages },
       } = getState();
 
-      if (pages[pageNumber].length === transactionsPerPage) {
+      if (pages[pageNumber].length === ordersPerPage) {
         return dispatch(changePage(pageNumber));
       }
 
-      const { id, created } = getOldestTransactionFromPages(pages);
+      const { sk } = getOldestOrderFromPages(pages);
 
-      const queryParams = `?orderAsc=false&limit=${transactionsPerPage}&exclusiveStartKey=${id}&compareTime=${encodeURIComponent(
-        created
-      )}`;
+      const queryParams = `?asc=false&limit=${ordersPerPage}&startSk=${encodeURIComponent(
+        sk
+      )}}`;
 
       const { data } = await axios.get(
-        `${REACT_APP_TRANSACTION_SERVICE_URL}/transaction/many${queryParams}`
+        `${REACT_APP_MEMESTOCK_API}/orders/${queryParams}`
       );
 
       dispatch(addPage({ pageNumber, data }));
@@ -112,10 +109,11 @@ function createPages(numPages) {
   return pages;
 }
 
-function getOldestTransactionFromPages(pages) {
-  const transactions = Object.values(pages).reduce((concatPages, page) => [
+function getOldestOrderFromPages(pages) {
+  const orders = Object.values(pages).reduce((concatPages, page) => [
     ...concatPages,
     ...page,
   ]);
-  return transactions[transactions.length - 1];
+
+  return orders[orders.length - 1];
 }
--- src/features/loginPage/LoginPage.js
+++ src/features/loginPage/LoginPage.js
@@ -5,30 +5,30 @@
 // show display if match
 
 // Imports
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {useForm} from 'react-hook-form';
-import {useDispatch} from 'react-redux';
-import {useHistory} from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 // Redux store
-import {updateUserInfo} from './userInfoSlice';
+import { updateUserInfo } from './usersInfoSlice';
 
 // Styles
 import styles from './LoginPage.module.css';
 
 // Components
-import {NavLink} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faUserPlus} from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
-const {REACT_APP_USER_SERVICE_URL} = process.env;
+const { REACT_APP_MEMESTOCK_API } = process.env;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 const usernameRegex = /[^\s]/;
 const emailRegex = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export function SignUpPage() {
-  const {register, handleSubmit, watch, reset, errors, setError} = useForm({
+  const { register, handleSubmit, watch, reset, errors, setError } = useForm({
     mode: 'onBlur',
     reValidateMode: 'onBlur',
   });
@@ -36,11 +36,10 @@ export function SignUpPage() {
   const dispatch = useDispatch();
   const history = useHistory();
 
-
   const onSignUpSubmit = async (data) => {
     try {
       console.log('onSignUpSubmit called', data);
-      const {username, email, password, passwordTwo} = data;
+      const { username, email, password, passwordTwo } = data;
       if (password !== passwordTwo) {
         setError('passwordTwo', {
           type: 'manual',
@@ -52,28 +51,37 @@ export function SignUpPage() {
       //   accountStatus: "FORCE_CHANGE_PASSWORD"
       //   email: "test123@test.com"
       const signupResponse = await axios.post(
-        `${REACT_APP_USER_SERVICE_URL}/user/signup`,
-        {username, email}
+        `${REACT_APP_MEMESTOCK_API}/users/signup`,
+        { username, email }
       );
 
       console.log('signupResponse', signupResponse);
-      const {accountStatus} = signupResponse.data;
+      const { accountStatus } = signupResponse.data;
 
       if (accountStatus === 'FORCE_CHANGE_PASSWORD') {
         // Finish creating user by setting the password
         const changePasswordResponse = await axios.post(
-          `${REACT_APP_USER_SERVICE_URL}/user/login`,
-          {username: email, password: 'NewpasS!23', newPassword: password}
+          `${REACT_APP_MEMESTOCK_API}/users/login`,
+          { username: email, password: 'NewpasS!23', newPassword: password }
         );
         if (changePasswordResponse) {
-          console.log('Signup was successful.  changePasswordResponse=', changePasswordResponse);
+          console.log(
+            'Signup was successful.  changePasswordResponse=',
+            changePasswordResponse
+          );
           // TODO: Refactor JSON response to use camelCase (on backend then remove this)
           const {
             AccessToken: accessToken,
             IdToken: idToken,
             RefreshToken: refreshToken,
           } = changePasswordResponse.data;
-          const payload = {accessToken, idToken, refreshToken, username, email};
+          const payload = {
+            accessToken,
+            idToken,
+            refreshToken,
+            username,
+            email,
+          };
           dispatch(updateUserInfo(payload));
           // Store token in session so other parts of app can get it
           // data.AccessToken,
@@ -84,14 +92,17 @@ export function SignUpPage() {
           //   RefreshToken: ""
           //   TokenType: "Bearer"
 
-          history.push("/feed");
+          history.push('/feed');
         } else {
-          console.log('request failed... changePasswordResponse=', changePasswordResponse);
+          console.log(
+            'request failed... changePasswordResponse=',
+            changePasswordResponse
+          );
         }
       }
-    } catch(error){
-      console.error('try/catch error', JSON.stringify(error,null, 4));
-      if (error && error.message && error.message.includes("status code 409")){
+    } catch (error) {
+      console.error('try/catch error', JSON.stringify(error, null, 4));
+      if (error && error.message && error.message.includes('status code 409')) {
         setError('username', {
           type: 'manual',
           message: 'Username already exists.  Please pick a different name.',
@@ -99,7 +110,10 @@ export function SignUpPage() {
       } else {
         setError('username', {
           type: 'manual',
-          message: 'Unexpected error, please refresh the page and try again. [' + error + ']',
+          message:
+            'Unexpected error, please refresh the page and try again. [' +
+            error +
+            ']',
         });
       }
     }
@@ -119,11 +133,9 @@ export function SignUpPage() {
           another one.
         </div>
 
-        {
-          errors.username &&  (
-            <div className={styles.error}>{errors.username.message}</div>
-          )
-        }
+        {errors.username && (
+          <div className={styles.error}>{errors.username.message}</div>
+        )}
 
         <label>
           Username:
@@ -139,11 +151,11 @@ export function SignUpPage() {
           />
         </label>
 
-        {
-          errors.email && (
-            <div className={styles.error}>Your password is missing or not formatted correctly.</div>
-          )
-        }
+        {errors.email && (
+          <div className={styles.error}>
+            Your password is missing or not formatted correctly.
+          </div>
+        )}
 
         <label>
           Email:
@@ -157,11 +169,13 @@ export function SignUpPage() {
             })}
           />
         </label>
-        {
-          errors.password && (
-            <div className={styles.error}>Your password is missing or not complex enough.  Must be 8 characters, 1 or more Upper, 1 or more lowercase and one or more number. </div>
-          )
-        }
+        {errors.password && (
+          <div className={styles.error}>
+            Your password is missing or not complex enough. Must be 8
+            characters, 1 or more Upper, 1 or more lowercase and one or more
+            number.{' '}
+          </div>
+        )}
         <label>
           Password:
           <input
@@ -176,11 +190,9 @@ export function SignUpPage() {
           />
         </label>
 
-        {
-          errors.passwordTwo &&  (
-            <div className={styles.error}>{errors.passwordTwo.message}</div>
-          )
-        }
+        {errors.passwordTwo && (
+          <div className={styles.error}>{errors.passwordTwo.message}</div>
+        )}
         <label>
           Re-enter your password:
           <input
@@ -188,7 +200,7 @@ export function SignUpPage() {
             type="password"
             className={errors.passwordTwo ? styles.inputError : styles.input}
             ref={register({
-              required: true
+              required: true,
             })}
           />
         </label>
@@ -205,7 +217,7 @@ export function SignUpPage() {
 }
 
 export function LoginPage(args) {
-  const {register, handleSubmit, watch, reset, setError, errors} = useForm({
+  const { register, handleSubmit, watch, reset, setError, errors } = useForm({
     mode: 'onBlur',
     reValidateMode: 'onBlur',
   });
@@ -214,31 +226,39 @@ export function LoginPage(args) {
   const history = useHistory();
 
   if (args.logOut) {
-    dispatch(updateUserInfo({email: "", username: "", accessToken:"", idToken:"", refreshToken:""}));
+    dispatch(
+      updateUserInfo({
+        email: '',
+        username: '',
+        accessToken: '',
+        idToken: '',
+        refreshToken: '',
+      })
+    );
   }
 
   const onSubmitLogin = async (data) => {
-    const {username, password} = data;
+    const { username, password } = data;
 
     console.log('onSubmitLogin called', data);
 
     try {
       const loginResponse = await axios.post(
-        `${REACT_APP_USER_SERVICE_URL}/user/login`,
-        {username, password}
+        `${REACT_APP_MEMESTOCK_API}/users/login`,
+        { username, password }
       );
 
       if (loginResponse) {
-        console.log("loginResponse", loginResponse);
+        console.log('loginResponse', loginResponse);
         // TODO: Refactor JSON response to use camelCase (on backend then remove this)
         const {
           AccessToken: accessToken,
           IdToken: idToken,
           RefreshToken: refreshToken,
         } = loginResponse.data;
-        const payload = {accessToken, idToken, refreshToken, username};
+        const payload = { accessToken, idToken, refreshToken, username };
         dispatch(updateUserInfo(payload));
-        history.push("/feed");
+        history.push('/feed');
       } else {
         reset();
       }
@@ -252,28 +272,33 @@ export function LoginPage(args) {
   };
 
   return (
-    <form className={styles.subFormContainer} onSubmit={handleSubmit(onSubmitLogin)}>
+    <form
+      className={styles.subFormContainer}
+      onSubmit={handleSubmit(onSubmitLogin)}
+    >
       <div className={styles.formContainer}>
         <h1>Sign In</h1>
         <div className={styles.login_message}>
           Please enter your username and password below.
-          <br/>
+          <br />
           If you don't have one, then sign up for free!
         </div>
-        {errors.username &&  (
+        {errors.username && (
           <div className={styles.error}>
             Username's are required and must be 4 or more characters
           </div>
         )}
         {errors.password && (
-          <div className={styles.error}>Your username or password is not valid</div>
+          <div className={styles.error}>
+            Your username or password is not valid
+          </div>
         )}
         {errors.password && errors.password.message && (
           <div className={styles.error}>{errors.password.message}</div>
         )}
 
-
-        <label htmlFor="username">Username:
+        <label htmlFor="username">
+          Username:
           <input
             name="username"
             className={errors.username ? styles.inputError : styles.input}
@@ -283,22 +308,19 @@ export function LoginPage(args) {
             })}
           />
         </label>
-        <label>Password:&nbsp;&nbsp;
+        <label>
+          Password:&nbsp;&nbsp;
           <input
             name="password"
             type="password"
             className={errors.password ? styles.inputError : styles.input}
             ref={register({
-              required: true
+              required: true,
             })}
           />
         </label>
         <div id="submit">
-          <input
-            type="submit"
-            value="Sign In"
-            className={styles.submit}
-          />
+          <input type="submit" value="Sign In" className={styles.submit} />
         </div>
         <div className={styles.signup_link}>
           <span>Don't have a login? &nbsp;&nbsp;</span>
